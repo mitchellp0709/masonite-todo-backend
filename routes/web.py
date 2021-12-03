f38948b23421771ea6846bd445480ac084f44666
Mitchell Paoletti <mitchellp0709@gmail.com>
2021-12-03 12:23:51 -0500

    first commit

---
+++ routes/web.py
@@ -0,0 +1,17 @@
+"""Web Routes."""
+
+from masonite.routes import Get, Post, Put, Delete, RouteGroup
+
+
+
+ROUTES = [
+    Get("/", "WelcomeController@show").name("welcome"),
+
+    RouteGroup([
+        Get("/", "TodoController@index").name("index"),
+        Get("/@id", "TodoController@show").name("show"),
+        Post("/","TodoController@create").name("create"),
+        Put("/@id","TodoController@update").name("update"),
+        Delete("/@id","TodoController@destroy").name("delete")
+    ], prefix="/todos", name="todos")
+]
